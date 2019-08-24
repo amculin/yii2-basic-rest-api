7b12610b965440041c5da63a348a9cc3de4a527f
Fahmi Auliya Tsani <fahmi@docotel.com>
2019-08-24 22:15:51 +0700

    Enable config for handling restAPI request

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Fu-gSNBgU0Jg-ZY1-u8tfFhIb3Vhk6-D',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -45,8 +48,10 @@ $config = [
         'db' => $db,
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'category'],
             ],
         ],
     ],
