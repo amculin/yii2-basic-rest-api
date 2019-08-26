8f6bd4f2f6eb571ee5a32885f5c8b940468a135a
Fahmi Auliya Tsani <fahmi@docotel.com>
2019-08-26 13:50:56 +0700

    feature-Update-productImage

--- controllers/ProductImageController.php
+++ controllers/ProductImageController.php
@@ -6,5 +6,15 @@ use yii\rest\ActiveController;
 class ProductImageController extends ActiveController
 {
     public $modelClass = 'app\models\ProductImage';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['create']['modelClass'] = 'app\models\Image';
+        $actions['create']['scenario'] = 'manual_upload';
+
+        return $actions;
+    }
 }
 ?>
--- models/Image.php
+++ models/Image.php
@@ -32,6 +32,7 @@ class Image extends BaseModel
     {
         return [
             [['name', 'file', 'enable'], 'required'],
+            [['name', 'file', 'enable'], 'required', 'on' => 'manual_upload'],
             [['enable'], 'integer'],
             [['name'], 'string', 'max' => 64],
             [['file'], 'string', 'max' => 255],
@@ -59,5 +60,43 @@ class Image extends BaseModel
         $productImage->product_id = $productId;
         $productImage->image_id = $this->id;
         $productImage->save();
+    }
+
+    public function preparedData()
+    {
+        $data = [];
+        $productId = Yii::$app->request->post('product_id');
+        $data['file'] = \yii\web\UploadedFile::getInstanceByName('images');
+        $data['product'] = Product::find()->where(['id' => $productId])->one();
+
+        return $data;
+    }
+
+    public function beforeValidate()
+    {
+        parent::beforeValidate();
+
+        $data = $this->preparedData();
+
+        $this->name = $data['product']->name;
+        $this->file = $data['file']->getBaseName() . time() . '.' . $data['file']->getExtension();
+        $this->enable = 1;
+
+        return true;
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        if ($insert && $this->scenario == 'manual_upload') {
+            $data = $this->preparedData();
+
+            $data['file']->saveAs($this::UPLOAD_PATH . $this->file);
+            $productImage = new ProductImage();
+            $productImage->image_id = $this->id;
+            $productImage->product_id = $data['product']->id;
+            $productImage->save();
+        }
     }
 }
